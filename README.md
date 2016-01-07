79b27107eb7c99a5d5863fe73a7b5af2626eadf9
Daniel Staudigel <dstaudigel@gmail.com>
2016-01-06 18:38:03 -0800

    Initial commit.

---
+++ README.md
@@ -0,0 +1,4 @@
+# ng-jsdata
+
+Utilities for managing state between angular and js-data.  Significant
+improvements in API over the standard js-data-angular model.
---
+++ index.js
@@ -0,0 +1,8 @@
+// Note, this syntax requires babel's syntax-export-extensions
+
+export * from './lib/on_change';
+export * from './lib/bind';
+export * from './lib/cache';
+
+
+
